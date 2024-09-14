10e01ef7320a921f047ddce5a574aa37f5683bab
rumaisa910 <167342257+rumaisa910@users.noreply.github.com>
2024-09-15 03:37:44 +0500

    Add files via upload

---
+++ milestone4/script.js
@@ -0,0 +1,23 @@
+// Get reference to the form and display area
+var form = document.getElementById('resume-form');
+var resumeDisplayElement = document.getElementById('resume-display');
+//Handle form submission 
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); //prevent page reload
+    //Collect input values
+    var name = document.getElementById('name').value;
+    var Email = document.getElementById('Email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    //Generate the resume content dynamically
+    var resumeHTML = "\n<h2><b>Editable Resume</b></h2>\n<h3>PERSONAL INFORMATION</h3>\n<p><b>Name:</b><span contenteditable=\"true\">".concat(name, "</span></p>\n<p><b>Email:</b><span contenteditable=\"true\">").concat(Email, "</span></p>\n<p><b>Phone:</b><span contenteditable=\"true\">").concat(phone, "</span></p>\n\n\n\n\n<h3>Education</h3>\n<p contenteditable=\"true\">").concat(education, "</p>\n\n<h3>Experience</h3>\n<p contenteditable=\"true\">").concat(experience, "</p>\n\n\n<h3>Skills</h3>\n<p contenteditable=\"true\">").concat(skills, "</p>\n\n");
+    //Display the generate resume
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('Fill Full Form To Generate.');
+    }
+});
